f35f3c02e6b60664aff52142a6dd05653385ced5
Akash Kulkarni <akashkulkarni@outlook.in>
2026-01-28 21:46:13 +0530

    Backend initialized

---
+++ rankup-backend/src/main/java/com/rankup/rankup_backend/RankupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rankup.rankup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankupBackendApplication.class, args);
+	}
+
+}
